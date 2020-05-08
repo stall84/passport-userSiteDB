689145a4224cd101bd4d17f7a243555aa2965e63
Michael Stallings <stall84@gmail.com>
2020-05-08 13:00:42 -0400

    initial commit with simple skeleton and dependencies

---
+++ config/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+module.exports = 
+{
+  development: {
+    username: "",
+    password: null,
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorAliases: false
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorAliases: false
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorAliases: false
+  }
+}
